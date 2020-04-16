a9c170fd70e1461d1851567d56becbf97136daaa
Marcos Corsi <marcosvcorsi@gmail.com>
2020-04-15 21:54:22 -0300

    WIP: User models, bcrypt and JWT

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -25,13 +25,13 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 // Dependency inversion principle
 
 interface Request {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
 
-    public async execute({ provider, date }: Request): Promise<Appointment> {
+    public async execute({ provider_id, date }: Request): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
         const appointmentDate = startOfHour(date);
@@ -42,7 +42,7 @@ class CreateAppointmentService {
             throw Error('This appointment is already booked');
         }
 
-        const appointment = appointmentsRepository.create({ provider, date: appointmentDate });
+        const appointment = appointmentsRepository.create({ provider_id, date: appointmentDate });
 
         await appointmentsRepository.save(appointment);
 
